5c884847dcc7065277a644e8008197a0e3c7e535
Sewex333 <168576408+Sewex333@users.noreply.github.com>
2025-03-03 17:24:25 +0100

    poprawki

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-Music lobby to aplikacja która skupia w jednym miejscu jedne z najlpeszych rapowych albumów 
+Music lobby to aplikacja która skupia w jednym miejscu jedne z najlpszych rapowych albumów 
 
 - mozliwosc utoworzenia swojego konta
 - kazdy uzytkownik ma mozliowsc dodania komentarza, ocenienia czy dodania swojego albumu do ulubionych
 - opcja penelu użytkownika gdzie są pokazane ulubione albumy czy ilość dodanych komentarzy czy ocen
 - każdy album ma rozpisaną trackliste oraz długość trwania każdego utworu
+
+- w przypadku gdyby coś nie działało w konsoli należy wpisać "python manage.py loaddata initial_data.json" aby załadować dane z pliku json
 
--- music_lobby/ap/apps.py
+++ music_lobby/ap/apps.py
@@ -1,21 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-def load_initial_data(sender, **kwargs):
-    from .models import Album
-    if not Album.objects.exists():
-        albums = [
-            {"title": "The Eminem Show", "artist": "Eminem", "release_year": 2001, "cover_image": "media/TES.jpg"},
-            {"title": "The Dark Side of the Moon", "artist": "Pink Floyd", "release_year": 1973, "cover_image": "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png"},
-            {"title": "Thriller", "artist": "Michael Jackson", "release_year": 1982, "cover_image": "https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png"},
-        ]
-        for album in albums:
-            Album.objects.create(**album)
 
 class ApConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ap'
-
-    def ready(self):
-        from django.db.models.signals import post_migrate
-        post_migrate.connect(load_initial_data, sender=self)
